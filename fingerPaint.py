313eb2aa21d24724c2612e684f98dfb227886c4b
Christian C <christianclark687@gmail.com>
2021-06-20 14:35:44 -0500

    can now paint on and clear canvas
    added functions to draw points on the canvas that have been previously traced by the tip of the pointer finger, calculate the current fps, and clear the screen when the 'c' key is pressed.

--- fingerPaint.py
+++ fingerPaint.py
@@ -3,47 +3,59 @@ import mediapipe as mp
 import time
 import hand_tracking as ht 
 
-pTime = 0 
-cTime = 0 
-cap = cv.VideoCapture(0)
-detector = ht.handDetector()
+canvasPoints = [] # [point-id, x, y]
 
 def rainbow():
-    return
+    return (255,0,0)
 
-def fingerPaint():
+def canvasDraw(points, img):
+    for point in points:
+        cv.circle(img, (point[1], point[2]), 10, rainbow(), cv.FILLED)
     return
 
 def clear():
+    canvasPoints.clear()
+    print(canvasPoints)
     return
 
 def eraser():
     return
 
+def framesPerSecond(past):
+    current = time.time()
+    fps = 1/(current-past)
+    past = current
+    return past, fps
+
 def main():
-    pTime = 0 
-    cTime = 0 
+    fps = 0
+    pTime = 0
     cap = cv.VideoCapture(0)
     detector = ht.handDetector()
-
+    
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
 
         lmList = detector.findPosition(img)
         if len(lmList) != 0 :
-            print(lmList[8])
+            fingertip = lmList[8]
+            canvasPoints.append(fingertip)
+            
+        imgResult = img.copy()
+        canvasDraw(canvasPoints, imgResult)
 
-        cTime = time.time()
-        fps = 1/(cTime-pTime)
-        pTime = cTime        
+        pTime, fps = framesPerSecond(pTime)
 
-        cv.putText(img, str(int(fps)), (10,70), cv.FONT_HERSHEY_PLAIN, 
+        cv.putText(imgResult, str(int(fps)), (10,70), cv.FONT_HERSHEY_PLAIN, 
                 3, (255,0,255), 3)
 
-        cv.imshow("Webcam", img)
+        cv.imshow("Webcam", imgResult)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
+        
+        if cv.waitKey(1) & 0xFF == ord('c'):
+            clear()
 
 if __name__ == '__main__':
     main()
